75629e780d3f8ed6b2658a295ee6be4fc0c95d68
Jongveloper <kjop7419@naver.com>
2023-01-13 02:22:49 +0900

    프로젝트 초기 세팅을 진행하라
    1. eslint 설정을 진행했습니다.
    2. emotion을 통해 reset CSS 설정을 진행했습니다.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Global } from "@emotion/react";
+import reset from "./reset";
+
+function App() {
+  return (
+    <div>
+      <Global styles={reset}/>
+      <p>GitHub Issue</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reset.tsx
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+
+const reset = css`
+  p {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+export default reset;
