31247e958f70a022bf52bba5cd11eb147073a1f7
Jongveloper <kjop7419@naver.com>
2023-01-13 14:57:34 +0900

    search페이지에서 사용할 Form 컴포넌트를 정의하라

---
+++ src/components/search/Form.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+import { Button, TextField } from '@mui/material';
+
+import CenterLayout from '../common/CenterLayout';
+
+const FlexForm = styled.form`
+  display: flex;
+`;
+
+const SearchInput = styled(TextField)`
+  width: 25rem;
+`;
+
+const SearchButton = styled(Button)`
+  margin-left: 1rem;
+
+  background-color: black;
+`;
+
+function Form() {
+  return (
+    <>
+      <CenterLayout>
+        <FlexForm>
+          <SearchInput placeholder="검색하고 싶은 레포지토리를 입력해주세요" />
+          <SearchButton variant="contained">검색</SearchButton>
+        </FlexForm>
+      </CenterLayout>
+    </>
+  );
+}
+
+export default Form;
