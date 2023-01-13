22cfbbb2912d7b03be9030752a2901454dfe3152
Jongveloper <kjop7419@naver.com>
2023-01-13 23:46:07 +0900

    언어에 따라 색을 return하는 함수를 생성하라

---
+++ src/utils/setCircleColor.ts
@@ -0,0 +1,55 @@
+export const setCircleColor = (language: string) => {
+  if (language === 'JavaScript') {
+    return '#FFF200';
+  }
+
+  if (language === 'TypeScript') {
+    return '#000080';
+  }
+
+  if (language === 'HTML') {
+    return '#FF4500';
+  }
+
+  if (language === 'CSS') {
+    return '#CC8899';
+  }
+
+  if (language === 'JAVA') {
+    return '#B5651D';
+  }
+
+  if (language === 'Python') {
+    return '#00008B';
+  }
+
+  if (language === 'Vue') {
+    return '228B22';
+  }
+
+  if (language === 'PHP') {
+    return 'ACACE6';
+  }
+
+  if (language === 'Ruby') {
+    return '#691734';
+  }
+
+  if (language === 'Swift') {
+    return '#FF5349';
+  }
+
+  if (language === 'Go') {
+    return 'skyblue';
+  }
+
+  if (language === 'C#') {
+    return 'green';
+  }
+
+  if (language === 'Jupyter Notebook') {
+    return '#DC5539';
+  }
+
+  return 'white';
+};
