c845d089e5c8cea60364c52ffdf09acdd0fcdf8c
Jongveloper <kjop7419@naver.com>
2023-01-14 21:25:43 +0900

    Loading 컴포넌트를 정의하라

---
+++ src/components/common/Loading/index.tsx
@@ -0,0 +1,13 @@
+import CircularProgress from '@mui/material/CircularProgress';
+
+import CenterLayout from '../CenterLayout';
+
+function Loading() {
+  return (
+    <CenterLayout>
+      <CircularProgress />
+    </CenterLayout>
+  );
+}
+
+export default Loading;
