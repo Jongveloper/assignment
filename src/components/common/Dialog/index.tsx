b94f0f9fa7b868a8af3154937ff29642c2632add
Jongveloper <kjop7419@naver.com>
2023-01-14 21:24:04 +0900

    Dialog를 정의하라
    메세지를 보여주기 위한 Dialog를 정의했습니다.

---
+++ src/components/common/Dialog/index.tsx
@@ -0,0 +1,48 @@
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import { DialogType } from '../../../redux/common';
+
+interface Props extends DialogType {
+  handleClose: () => void;
+  handleNavigate: () => void;
+}
+
+export default function CommonDialog({
+  isOpen,
+  message,
+  title,
+  status,
+  handleClose,
+  handleNavigate,
+}: Props) {
+  return (
+    <div>
+      <Dialog
+        open={isOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          {status === 'ALERT' && <Button onClick={handleNavigate}>북마크 보러가기</Button>}
+          <Button onClick={handleClose} autoFocus>
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
