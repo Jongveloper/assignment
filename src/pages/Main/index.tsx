b92a515d3b54a9bd87c7dc8e00279b0ef6c1d8fa
Jongveloper <kjop7419@naver.com>
2023-01-14 21:26:41 +0900

    MainPage를 완성하라
    1. RepositoriesContainer를 추가하여 repository검색 후 repository가 나올 수 있도록 했습니다.
    2. Loading Cotainer를 통해 로딩 중일 때 로딩 스피너가 도는 것을 볼 수 있게 했습니다.
    3. Dialog Contaienr를 통해 전달할 메세지가 있을 때 Dialog를 볼 수 있게 했습니다.

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,15 @@
+import DialogContainer from '../../containers/common/DialogContainer';
+import LoadingContainer from '../../containers/common/LoadingContainer';
 import FormContainer from '../../containers/main/FormContainer';
+import RepositoriesContainer from '../../containers/main/RepositoriesContainer';
 
 function Main() {
   return (
     <>
       <FormContainer />
+      <RepositoriesContainer />
+      <LoadingContainer />
+      <DialogContainer />
     </>
   );
 }
