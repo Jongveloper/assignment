0fb6d8ae96b1e0abe6ff9a1698819f98b19ed9ad
Jongveloper <kjop7419@naver.com>
2023-01-14 21:12:31 +0900

    repository를 받아오는 비동기 처리를 하는 함수를 생성하라
    1. 401에러는 github_token 에러인데 유저 쪽에서 처리를 할 수 없는 에러이기 때문에 내부적인 오류로 관리자에게 문의하라는 메세지를 남겼습니다.
    2. 422에러는 validation 에러이기 때문에 검색어를 입력하라는 메세지를 남겼습니다.
    3. 503에러는 서버 에러이기 때문에 서비스가 불가하다는 메시지를 남겼습니다.
    4. 문서에 나와있지 않은 status code는 어떤 오류가 발생했는 지 알 수 없기 때문에 예기치 못한 오류가 발생했다는 메세지를 남겼습니다.

---
+++ src/service/getRepository.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosError } from 'axios';
+
+import { RequestRepositoriesProps } from '../types/Repository';
+
+interface ServerError {
+  errors: { status: string, field: string, message: string }[];
+}
+
+class AuthorizationError extends Error {
+  constructor() {
+    super('내부적인 오류가 발생했습니다. 관리자에게 문의해주세요.');
+  }
+}
+
+class ValidationError extends Error {
+  constructor() {
+    super('검색어를 입력해주세요.')
+  }
+}
+
+class ServiceError extends Error {
+  constructor() {
+    super('서비스가 불가합니다. 잠시 후 다시 이용해주세요.');
+  }
+}
+
+class UnknownError extends Error {
+  constructor() {
+    super('예기치 못한 오류가 발생했습니다. 잠시 후 다시 이용해주세요.');
+  }
+}
+
+export const getRepository = async ({ repository, page } : RequestRepositoriesProps) => {
+  const baseURL = import.meta.env.VITE_APP_API_URL;
+  try {
+    const data = await axios.get(`${baseURL}/search/repositories?q=${repository}&sort=starts&order=desc&per_page=10&page=${page}`, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_TOKEN}`,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    const status = (error as AxiosError<ServerError>).response?.status;
+
+    if (status === 401) {
+      throw new AuthorizationError();
+    }
+
+    if (status === 422) {
+      throw new ValidationError();
+    }
+
+    if (status === 503) {
+      throw new ServiceError();
+    }
+
+    throw new UnknownError();
+  }
+};
