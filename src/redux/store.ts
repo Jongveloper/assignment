ecb10a9bcd3ed76f0e1c4cb89a2d76d6d6d122d6
Jongveloper <kjop7419@naver.com>
2023-01-14 21:23:05 +0900

    store에 사용할 slice를 등록하라
    1. bookmark slice를 등록했습니다.
    2. common slice를 등록했습니다.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,10 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
+import bookmarkSlice from './bookmark';
+import commonSlice from './common';
 import repositorySlice from './repository';
 
 export const store = configureStore({
   reducer: {
+    bookmark: bookmarkSlice,
+    common: commonSlice,
     repository: repositorySlice,
   },
 });
