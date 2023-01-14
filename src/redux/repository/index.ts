99112a5abddd519c838502db0aa974b855201103
Jongveloper <kjop7419@naver.com>
2023-01-14 21:18:36 +0900

    repositorySlice를 생성하라
    1. setSearchWord로 searchInput에 대한 상태를 핸들링할 수 있습니다.
    2. saveRepositories를 통해 비동기 요청을 통해 얻은 데이터를 저장할 수 있습니다.
    page를 2로 세팅하는 것은 무한 스크롤을 하기 위해 page를 2로 변경합니다.
    3. saveMoreRepositories를 통해 데이터를 추가로 저장할 수 있습니다.

---
+++ src/redux/repository/index.ts
@@ -0,0 +1,120 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { AppDispatch } from '../store';
+
+import { getRepository } from '../../service/getRepository';
+
+import { convertRepositories } from '../../utils/convertRepositories';
+
+import { RequestRepositoriesProps } from '../../types/Repository';
+
+import { setDialog, setLoading } from '../common';
+
+export interface IssuesUrlInfo {
+  owner: string;
+  repo: string;
+}
+
+export interface RepositoryInfo {
+  id: number;
+  full_name: string;
+  updated_at: string;
+  description: string;
+  language?: string;
+  stargazers_count?: number | string;
+  circleColor: string;
+  issues_url: IssuesUrlInfo;
+}
+
+interface Repository {
+  repositories: RepositoryInfo[];
+  searchWord: string;
+  page: number;
+}
+
+export const initialState:Repository = {
+  repositories: [],
+  searchWord: '',
+  page: 0,
+};
+
+const { actions, reducer } = createSlice({
+  name: 'repository',
+  initialState,
+  reducers: {
+    setSearchWord: (state, { payload }) => ({
+      ...state,
+      searchWord: payload,
+    }),
+    saveRepositories: (state, { payload }) => {
+      const filteredRepositories = convertRepositories(payload);
+      return {
+        ...state,
+        repositories: filteredRepositories,
+        page: 2,
+      };
+    },
+    saveMoreRepositories: (state, { payload }) => {
+      const filteredRepositories = convertRepositories(payload);
+      return {
+        ...state,
+        repositories: [...state.repositories, ...filteredRepositories],
+        page: state.page + 1,
+      };
+    },
+  },
+});
+
+export const {
+  setSearchWord,
+  saveRepositories,
+  saveMoreRepositories,
+} = actions;
+
+export default reducer;
+
+export const loadRepositories = ({ repository, page } :
+  RequestRepositoriesProps) => async (dispatch: AppDispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const { data: { items } } = await getRepository({ repository, page });
+
+    dispatch(saveRepositories(items));
+
+    dispatch(setLoading(false));
+  } catch (error) {
+    const { message } = error as Error;
+
+    dispatch(setLoading(false));
+
+    dispatch(setDialog({
+      isOpen: true,
+      status: 'ERROR',
+      message,
+      title: '레포지토리를 불러오는데 실패했습니다.',
+    }));
+  }
+};
+
+export const loadMoreRepositories = ({ repository, page } :
+  RequestRepositoriesProps) => async (dispatch: AppDispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const { data: { items } } = await getRepository({ repository, page });
+
+    dispatch(saveMoreRepositories(items));
+
+    dispatch(setLoading(false));
+  } catch (error) {
+    const { message } = error as Error;
+
+    dispatch(setLoading(false));
+
+    dispatch(setDialog({
+      isOpen: true,
+      status: 'ERROR',
+      message,
+      title: '레포지토리를 불러오는데 실패했습니다.',
+    }));
+  }
+};
