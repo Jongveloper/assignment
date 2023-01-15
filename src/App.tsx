8058a06544aea637021b84bb01f8c8a0cde991ad
Jongveloper <kjop7419@naver.com>
2023-01-16 01:14:52 +0900

    bookmark페이지에 진입할 수 있게하라

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Global } from '@emotion/react';
 import { Route, Routes } from 'react-router-dom';
+
+import Bookmark from './pages/Bookmark';
 import Main from './pages/Main';
 
 import reset from './reset';
@@ -10,6 +12,8 @@ function App() {
       <Global styles={reset} />
       <Routes>
         <Route index element={<Main />} />
+        <Route path="/bookmark/*" element={<Bookmark />} />
+        <Route path="/bookmark/:repositoryId" element={<Bookmark />} />
       </Routes>
     </>
   );
