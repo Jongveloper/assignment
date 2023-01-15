613bff7b748b2b15dda8606c20b270dc45fec27d
Jongveloper <kjop7419@naver.com>
2023-01-16 02:48:01 +0900

    북마크가 없을 때 보여줄 컴포넌트를 생성하라

---
+++ src/components/bookmark/NoContents/index.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+import Button from '@mui/material/Button';
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 70rem;
+  margin: auto;
+
+  text-align: center;
+`;
+
+const NavigateButton = styled(Button)`
+  background-color: black;
+
+  :hover {
+    background-color: white;
+    color: black;
+  }
+`;
+
+export default function NoContents({
+  handleNavigate,
+}:{handleNavigate : () => void}) {
+  return (
+    <Wrap>
+      <div>
+        <h1>북마크에 저장된게 없어요!</h1>
+        <NavigateButton
+          variant="contained"
+          onClick={handleNavigate}
+        >
+          검색하러 가기
+        </NavigateButton>
+      </div>
+    </Wrap>
+  );
+}
