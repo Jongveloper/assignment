8ae19ebf16a120912d9e0216ab718bcae89893fd
Jongveloper <kjop7419@naver.com>
2023-01-16 01:03:53 +0900

    로컬스토리지에 북마크를 저장할 수 있는 함수를 생성하라

--- src/utils/localStorageSetBookmark.ts
+++ src/utils/localStorageSetBookmark.ts
@@ -1,17 +1,15 @@
-import { RepositoryInfo } from "../redux/repository/type";
+import { RepositoryInfo } from '../redux/repository/type';
 
-import { Issue } from "../redux/bookmark/type";
+import { Issue } from '../redux/bookmark/type';
 
 export const localStorageSetBookmark = (issues: Issue[], repository: RepositoryInfo) => {
   const bookmark = localStorage.getItem('bookmark');
 
-  if(bookmark === null) {
-    localStorage.setItem('bookmark', JSON.stringify([{repository, issues}]));
+  if (bookmark === null) {
+    localStorage.setItem('bookmark', JSON.stringify([{ repository, issues }]));
 
     return;
   }
 
-  localStorage.setItem('bookmark', JSON.stringify([...JSON.parse(bookmark), {repository ,issues}]));
-
-  return;
-}
+  localStorage.setItem('bookmark', JSON.stringify([...JSON.parse(bookmark), { repository, issues }]));
+};
