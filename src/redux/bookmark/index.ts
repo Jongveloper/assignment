9c2d8f59a67ca571d8a3c0f7aaa613311e616245
Jongveloper <kjop7419@naver.com>
2023-01-16 01:06:26 +0900

    bookmark를 리팩토링하고 북마크를 선택하는 함수를 생성하라
    1. 기존 duplicateDetection 함수명이 명확하지 않아 alreadyExistsIn으로 변경한 변경 사항을 적용했습니다.
    2. 기존 순환 참조 에러가 발생하여 인자로 alreadyExistsIn에서 store를 호출하지 않고 bookmarks를 인자로 넘기는 형식으로 변경한 사항을 적용했습니다.
    3. 북마크 최대 수는 변경될 수 있는 사항이기 때문에 하드코딩되어있던 부분을 제거하고 변수화 시켰습니다.

--- src/redux/bookmark/index.ts
+++ src/redux/bookmark/index.ts
@@ -2,46 +2,65 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { getIssue } from '../../service/getIssue';
 
-import { convertIssue } from "../../utils/convertIssue";
-import { duplicateDetection } from '../../utils/duplicateDetection';
-import { localStorageSetBookmark } from "../../utils/localStorageSetBookmark";
+import { convertIssue } from '../../utils/convertIssue';
+import { alreadyExistsIn } from '../../utils/alreadyExistsIn';
+import { localStorageSetBookmark } from '../../utils/localStorageSetBookmark';
 
 import { setDialog } from '../common';
 
 import { AppDispatch, RootState } from '../store';
 
-import { Bookmark, Bookmarks, loadIssueProps } from "./type";
+import { Bookmark } from './type';
 
-export const initialState: Bookmark = {
+const MAX_BOOKMARKS_SIZE = 4;
+
+interface BookmarkState {
+  bookmarks: Bookmark[];
+  selectedBookmark?: Bookmark;
+}
+
+export const initialState: BookmarkState = {
   bookmarks: JSON.parse(localStorage.getItem('bookmark') as string) ?? [],
+  selectedBookmark: undefined,
 };
 
 const { actions, reducer } = createSlice({
   name: 'bookmark',
   initialState,
   reducers: {
-    saveBookmark: (state, { payload }) => {
-      const bookmark = { repository: payload.repository, issues: payload.issues };
+    saveBookmarks: (state, { payload: bookmark }) => ({
+      ...state,
+      bookmarks: [...state.bookmarks, bookmark],
+    }),
+    selectBookmark: (state, { payload: id }) => {
+      if (state.selectedBookmark?.repository.id === id) {
+        return state;
+      }
+
+      const bookmark = state.bookmarks
+        .find(({ repository }) => repository.id === id);
 
       return {
         ...state,
-        bookmarks: [...state.bookmarks, bookmark] as Bookmarks[],
+        selectedBookmark: bookmark,
       };
     },
   },
 });
 
 export const {
-  saveBookmark,
+  saveBookmarks,
+  selectBookmark,
 } = actions;
 
-export default reducer;
-
-export const setBookmark = ({ owner, repo, repository }
-  : loadIssueProps) => async (dispatch: AppDispatch, getState: () => RootState) => {
+export const setBookmark = ({
+  owner,
+  repo,
+  repository,
+}: any) => async (dispatch: AppDispatch, getState: () => RootState) => {
   const { bookmark: { bookmarks } } = getState();
 
-  if (duplicateDetection(repository)) {
+  if (alreadyExistsIn(bookmarks, repository)) {
     dispatch(setDialog({
       isOpen: true,
       status: 'ALERT',
@@ -52,11 +71,11 @@ export const setBookmark = ({ owner, repo, repository }
     return;
   }
 
-  if (bookmarks.length >= 4) {
+  if (bookmarks.length >= MAX_BOOKMARKS_SIZE) {
     dispatch(setDialog({
       isOpen: true,
       status: 'ALERT',
-      message: '북마크는 최대 4개까지 저장할 수 있습니다.',
+      message: `북마크는 최대 ${MAX_BOOKMARKS_SIZE}개까지 저장할 수 있습니다.`,
       title: '북마크 저장에 실패했습니다.',
     }));
 
@@ -68,7 +87,7 @@ export const setBookmark = ({ owner, repo, repository }
 
     const filteredIssue = convertIssue(data);
 
-    dispatch(saveBookmark({ issues: filteredIssue, repository }));
+    dispatch(saveBookmarks({ issues: filteredIssue, repository }));
 
     localStorageSetBookmark(filteredIssue, repository);
 
@@ -89,3 +108,5 @@ export const setBookmark = ({ owner, repo, repository }
     }));
   }
 };
+
+export default reducer;
