56f2e0523a65c54cae1ba41ce700e9764e39b4f6
Jongveloper <kjop7419@naver.com>
2023-01-16 07:17:08 +0900

    비동기 요청 발생 시 유저가 로딩 상태일 때 로딩 상태를 알 수 있게하라

--- src/pages/Bookmark/index.tsx
+++ src/pages/Bookmark/index.tsx
@@ -1,11 +1,13 @@
 import IssueContainer from '../../containers/bookmark/IssueContainer';
 import TabsContainer from '../../containers/bookmark/TabsContainer';
+import LoadingContainer from '../../containers/common/LoadingContainer';
 
 function Bookmark() {
   return (
     <>
       <TabsContainer />
       <IssueContainer />
+      <LoadingContainer />
     </>
   );
 }
