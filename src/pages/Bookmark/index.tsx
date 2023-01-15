3da3c8c1beb2b80412fd134ada2a4f811eca8197
Jongveloper <kjop7419@naver.com>
2023-01-16 01:14:36 +0900

    Bookmark페이지를 퍼블리싱 하라

---
+++ src/pages/Bookmark/index.tsx
@@ -0,0 +1,13 @@
+import IssueContainer from '../../containers/bookmark/IssueContainer';
+import TabsContainer from '../../containers/bookmark/TabsContainer';
+
+function Bookmark() {
+  return (
+    <>
+      <TabsContainer />
+      <IssueContainer />
+    </>
+  );
+}
+
+export default Bookmark;
