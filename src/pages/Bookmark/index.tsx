8d30f9f246d57978e819f206280f454f780a013f
Jongveloper <kjop7419@naver.com>
2023-01-16 13:55:22 +0900

    Header를 북마크 페이지에서 사용하라

--- src/pages/Bookmark/index.tsx
+++ src/pages/Bookmark/index.tsx
@@ -1,10 +1,12 @@
 import IssueContainer from '../../containers/bookmark/IssueContainer';
 import TabsContainer from '../../containers/bookmark/TabsContainer';
+import HeaderContainer from '../../containers/common/HeaderContainer';
 import LoadingContainer from '../../containers/common/LoadingContainer';
 
 function Bookmark() {
   return (
     <>
+      <HeaderContainer />
       <TabsContainer />
       <IssueContainer />
       <LoadingContainer />
