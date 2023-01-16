a98f9fbde63d67b21049655af4e4f5676b5634f2
Jongveloper <kjop7419@naver.com>
2023-01-16 11:26:25 +0900

    사용할 데이터를 추가가공 하라
    해당하는 IssuePage로 이동하기 위해 url 데이터도 리턴하도록 변경하였습니다.

--- src/utils/convertIssue.ts
+++ src/utils/convertIssue.ts
@@ -1,11 +1,14 @@
-import { Issue } from "../types/Issue";
+import { Issue } from '../types/Issue';
 
 export const convertIssue = (issues: Issue[]) => issues.map(({
-  user:{ login },
+  user: { login },
   title,
-  body
+  body,
+  html_url,
 }: Issue) => {
-  const data = { user : login, title, body }
+  const data = {
+    user: login, title, body, url: html_url,
+  };
 
   return data;
-})
+});
