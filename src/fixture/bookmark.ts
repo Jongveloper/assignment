abe4ce1cd7b1bf09398a5cc16a273ae37659a7b7
Jongveloper <kjop7419@naver.com>
2023-01-18 01:47:21 +0900

    bookmarks fixture 데이터를 추가하라

--- src/fixture/bookmark.ts
+++ src/fixture/bookmark.ts
@@ -31,3 +31,37 @@ export const bookmark: Bookmark = {
     url: 'testUrl3',
   }],
 };
+
+export const bookmarks = [
+  {
+    repository: {
+      id: 1,
+      fullName: 'tester',
+      updatedAt: '2023-01-18',
+      description: 'testing',
+      language: 'JavaScript',
+      stargazersCount: '23',
+      circleColor: 'yellow',
+      owner: 'owner',
+      repo: 'repo',
+    },
+    issues: [{
+      user: 'tester1',
+      title: 'Issue',
+      body: 'IssueBody',
+      url: 'testUrl',
+    },
+    {
+      user: 'tester2',
+      title: 'Issue2',
+      body: 'IssueBody2',
+      url: 'testUrl2',
+    },
+    {
+      user: 'tester3',
+      title: 'Issue3',
+      body: 'IssueBody3',
+      url: 'testUrl3',
+    }],
+  },
+];
