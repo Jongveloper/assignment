c0b5036f6e311f4d5e0e8051ad75d8fbf10c3b45
Jongveloper <kjop7419@naver.com>
2023-01-19 16:35:33 +0900

    convertIssue를 리팩토링해라
    한개로 만들 수 있는 함수를 두개로 나눈 것이 어색하다고 생각하여 합치는 작업을 진행했습니다.

--- src/utils/convertIssue.ts
+++ src/utils/convertIssue.ts
@@ -1,28 +1,20 @@
 import { Issue } from '../types/Issue';
 
-export const convertIssue = (issues: Issue[]) => {
-  const filteredIssue = issues.filter(({
-    html_url,
-  }: Issue) => {
+export const convertIssue = (issues: Issue[]) => issues
+  .filter(({ html_url }: Issue) => {
     const splitUrl = html_url.split('/');
 
     if (splitUrl[splitUrl.length - 2] === 'issues') {
       return issues;
     }
-  });
-
-  const processedIssue = filteredIssue.map(({
+  }).map(({
     user: { login },
     title,
     body,
     html_url,
-  }: Issue) => {
-    const data = {
-      user: login, title, body, url: html_url,
-    };
-
-    return data;
-  });
-
-  return processedIssue;
-};
+  }: Issue) => ({
+    user: login,
+    title,
+    body,
+    url: html_url,
+  }));
